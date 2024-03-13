3a3766f9b82251a5fec66295c8405eb5518dfa48
IsabelBelosa <isabel.becerralosada@alum.uca.es>
2024-03-13 19:48:32 +0100

    cambios

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float velocidad = 5.0f;
+    public float valorherida = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movDistancia = Time.deltaTime* velocidad;
+        
+    }
+}
